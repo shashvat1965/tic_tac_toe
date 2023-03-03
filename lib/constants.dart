838b0a41e5db70739609e2d0758d367ae27186b8
shashvat1965 <shashvat1965@gmail.com>
2023-03-03 19:04:17 +0530

    Initial Commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,34 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 Color kBlueColour = const Color(0xFF4285f4);
 Color kGreenColor = const Color(0xFF33a853);
 Color kRedColor = const Color(0xFFe94234);
 Color kYellowColor = const Color(0xFFfabc01);
-
-class ColorConstants{
-  static List<Color> colourList = [kBlueColour, kGreenColor, kRedColor, kYellowColor];
-
-  void randomiseTheme(){
-    colourList.shuffle();
-  }
-}
-
-getSnackBar(String text, BuildContext context) {
-  var snackBar = SnackBar(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(10),
-    ),
-    behavior: SnackBarBehavior.floating,
-    backgroundColor: Colors.grey,
-    duration: const Duration(seconds: 5),
-    content: SizedBox(
-        height: 25,
-        child: Center(
-            child: Text(
-              text,
-              style: GoogleFonts.nunito(color: Colors.black, fontSize: 20),
-            ))),
-  );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-}
--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:tic_tac_toe/screens/grid_screen.dart';
-
-import '../constants.dart';
 
 class StartScreen extends StatefulWidget {
   const StartScreen({Key? key}) : super(key: key);
@@ -12,119 +8,11 @@ class StartScreen extends StatefulWidget {
 }
 
 class _StartScreenState extends State<StartScreen> {
-  String? playerAName;
-  String? playerBName;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: true,
         backgroundColor: Colors.black,
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 40, left: 20),
-                  child: Row(
-                    children: [
-                      GestureDetector(
-                          onTap: () {
-                            ColorConstants().randomiseTheme();
-                            setState(() {});
-                          },
-                          child: Icon(
-                            Icons.color_lens_outlined,
-                            color: ColorConstants.colourList[2],
-                          ))
-                    ],
-                  ),
-                ),
-                Padding(
-                    padding: const EdgeInsets.only(top: 210, bottom: 40),
-                    child: Text(
-                      "Tic-Tac-Toe",
-                      style: GoogleFonts.nunito(
-                          fontSize: 60, color: ColorConstants.colourList[0]),
-                    )),
-                SizedBox(
-                  width: 250,
-                  child: TextFormField(
-                    cursorColor: ColorConstants.colourList[1],
-                    style:
-                        GoogleFonts.nunito(color: ColorConstants.colourList[1], fontSize: 15),
-                    decoration: InputDecoration(
-                        labelStyle: GoogleFonts.nunito(
-                            color: ColorConstants.colourList[1]),
-                        labelText: "Player A Name",
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5),
-                            borderSide: BorderSide(
-                                color: ColorConstants.colourList[1])),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5),
-                            borderSide: BorderSide(
-                                color: ColorConstants.colourList[1]))),
-                    onChanged: (value) {
-                      playerAName = value;
-                    },
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  width: 250,
-                  child: TextFormField(
-                    cursorColor: ColorConstants.colourList[2],
-                    style:
-                        GoogleFonts.nunito(color: ColorConstants.colourList[2], fontSize: 15),
-                    decoration: InputDecoration(
-                        labelStyle: GoogleFonts.nunito(
-                            color: ColorConstants.colourList[2]),
-                        labelText: "Player B Name",
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5),
-                            borderSide: BorderSide(
-                                color: ColorConstants.colourList[2])),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5),
-                            borderSide: BorderSide(
-                                color: ColorConstants.colourList[2]))),
-                    onChanged: (value) {
-                      playerBName = value;
-                    },
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: ColorConstants.colourList[0],
-                    ),
-                    onPressed: () {
-                      if (playerAName != null && playerBName != null) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => GridScreen(
-                                      playerAName: playerAName!,
-                                      playerBName: playerBName!,
-                                    )));
-                      } else {
-                        getSnackBar("Please enter both the fields", context);
-                      }
-                    },
-                    child: Text(
-                      "Start",
-                      style: GoogleFonts.nunito(
-                          fontSize: 20, color: ColorConstants.colourList[3]),
-                    ))
-              ],
-            ),
-          ),
-        ));
+        body: Container());
   }
 }
